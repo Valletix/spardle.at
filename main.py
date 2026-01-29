f941030915feed833473294e7ecef0da3a47c48f
Valentin Kaltenegger <Valltix23@gmail.com>
2026-01-29 14:13:58 +0100

    basic functions for getting the data of one page – Images don't work yet

---
+++ main.py
@@ -0,0 +1,48 @@
+import asyncio
+import pandas as pd
+from pydoll.browser.chromium import Chrome
+from pydoll.constants import Key
+
+product_df = pd.DataFrame(columns=["product_brand", "product_name", "product_weight", "product_price"])
+
+def normalize_strings(text):
+    not_wanted_chars = ["\\", "/", "*", "?", "!", "<", ">", "\""]
+    new_text = "".join(filter(lambda c: c not in not_wanted_chars, text))
+    return new_text
+            
+
+async def find_products():
+    async with Chrome() as browser:
+        tab = await browser.start()
+        await tab.go_to('https://www.spar.at/produktwelt/lebensmittel?page=1', timeout=5)
+
+        await tab.keyboard.press(Key.ESCAPE)
+
+        products = await tab.find(class_name="spar-plp__grid-item", find_all=True)
+
+        for product in products:
+            brand = await product.find(class_name="product-tile__name1")
+            name = await product.find(class_name="product-tile__name2")
+            weight = await product.find(class_name="product-tile__name3", raise_exc=False)
+            old_price = await product.find(class_name="product-price__price-old", raise_exc=False)
+            if old_price:
+                price = old_price
+            else:
+                price = await product.find(class_name="product-price__price")
+            img = await product.find(class_name="adaptive-image__img")
+
+            brand_text = await brand.text
+            name_text = await name.text
+            if weight:
+                weight_text = await weight.text
+            else:
+                weight_text = None
+            price_text = await price.text
+
+            await product.take_screenshot(f"img/{normalize_strings(f"{brand_text}_{name_text}_{weight_text}")}.png", quality=85)
+
+
+            product_df.loc[len(product_df)] = [brand_text, name_text, weight_text, price_text]
+    print(product_df)
+
+asyncio.run(find_products())
