51746c2579b816a1cf424b5341c05e87c7ab92f4
Valentin Kaltenegger <Valltix23@gmail.com>
2026-02-06 12:21:45 +0100

    changed guesses to multiple fields, some bug fixing, some css

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+import confetti from "https://cdn.jsdelivr.net/npm/canvas-confetti@1.6.0/+esm";
+
 async function get_products() {
     const request = new Request("/jsons/products_cleaned.json");
     const response = await fetch(request);
@@ -14,7 +16,7 @@ function choose_product(product_json) {
 function show_product_info(product) {
     
     const section = document.querySelector("section");
-    const img_section = document.querySelector("img-section");
+    const img_section = document.querySelector("img_section");
     
     const product_img = document.createElement("img");
     const product_brand = document.createElement("div");
@@ -29,7 +31,7 @@ function show_product_info(product) {
     product_img.height = 200;
     product_brand.textContent = "Marke: " + product.product_brand;
     product_name.textContent = "Produkt: " + product.product_name;
-    product_weight.textContent = "Gewicht: " + product.product_weight;
+    product_weight.textContent = "Menge: " + product.product_weight;
     section.appendChild(product_brand);
     section.appendChild(product_name);
     section.appendChild(product_weight);
@@ -42,6 +44,11 @@ function add_event_listeners(product) {
     input_field.addEventListener("focus", () => {
         input_field.setAttribute("placeholder", "0.00");
     })
+    input_field.addEventListener("keydown", function(event) {
+        if(event.key === "Enter") {
+            submit_button.click();
+        }
+    })
     input_field.addEventListener("blur", () => {
         input_field.setAttribute("placeholder", "Gib einen Tipp ab...")
     })
@@ -51,29 +58,104 @@ function add_event_listeners(product) {
 }
 
 function make_guess(product) {
+
+    const guess_section = document.querySelector("guess_section");
+    const guess_grid = document.createElement("div");
+    const guess = document.createElement("div")
+    const direction = document.createElement("div");
+
+    guess_grid.classList.add("guess_grid");
+    guess.classList.add("guess");
+    direction.classList.add("direction")
     const guess_price = document.getElementById("input_guess").value;
+
     if (guess_price == "") {
         no_input();
         return
     }
-    const guess_section = document.querySelector("guess-section");
-    const guess = document.createElement("div");
-    guess.classList.add("guess");
-    if (product.product_price > guess_price){
-        guess.textContent = `${guess_price} – Zu niedrig!`
-    }
-    else if (product.product_price < guess_price) {
-        guess.textContent = `${guess_price} – Zu hoch!`
+   
+    if(product.product_price != guess_price) {
+        incorrect_guess(guess_section, guess_grid, guess, direction, guess_price, product.product_price);
     }
     else {
-        guess.textContent = `${guess_price} – Passt genau!`
-        disable_inputs()
-    }
-    guess_section.appendChild(guess)
-    if (guess_section.children.length >= 6) {
-        disable_inputs()
+        correct_guess(guess_section, guess_grid, guess, direction, guess_price);
     }
 }
+
+function incorrect_guess(
+    guess_section, 
+    guess_grid,
+    guess,
+    direction,
+    guess_price, 
+    product_price) {
+
+    if (product_price*0.75 > guess_price){
+        guess.textContent = `€ ${guess_price}`;
+        direction.textContent = "🔼";
+        guess_grid.appendChild(guess);
+        guess_grid.appendChild(direction);
+    }
+    else if (product_price > guess_price) {
+        guess.textContent = `€ ${guess_price}`;
+        direction.textContent = "🔼"
+        direction.classList.add("close")
+        guess_grid.appendChild(guess);
+        guess_grid.appendChild(direction);
+        
+    }
+    else if (product_price*1.25 < guess_price) {
+        guess.textContent = `€ ${guess_price}`;
+        direction.textContent = "🔽"
+        guess_grid.appendChild(guess);
+        guess_grid.appendChild(direction);
+    }
+    else if (product_price < guess_price) {
+        guess.textContent = `€ ${guess_price}`;
+        direction.textContent = "🔽"
+        direction.classList.add("close")
+        guess_grid.appendChild(guess);
+        guess_grid.appendChild(direction);
+    }
+
+    guess_section.appendChild(guess_grid)
+
+    if (guess_section.children.length >= 6) {
+        disable_inputs();
+        show_solution(product_price);
+    }
+}
+
+function correct_guess(
+    guess_section, 
+    guess_grid, 
+    guess, 
+    direction, 
+    guess_price) {
+
+    guess.textContent = `€ ${guess_price}`;
+    direction.textContent = "✅";
+    direction.classList.add("correct");
+    guess_grid.appendChild(guess);
+    guess_grid.appendChild(direction);
+    confetti({
+        particleCount: 200,
+        spread: 70,
+        origin: { y: 0.6 },
+    });
+    disable_inputs();
+    guess_section.appendChild(guess_grid);
+}
+
+function show_solution(product_price) {
+    const guess_section = document.querySelector("guess_section");
+    const solution = document.createElement("div");
+    solution.classList.add("guess", "correct");
+    solution.textContent = `Lösung: ${product_price}`;
+    guess_section.appendChild(solution);
+    return
+}
+
 function disable_inputs() {
     const submit_button = document.getElementById("submit_button")
     const input_field = document.getElementById("input_guess")
@@ -94,11 +176,6 @@ async function main() {
     add_event_listeners(product);
 }
 
-
-
-
-
-
-
+main()
 
 
